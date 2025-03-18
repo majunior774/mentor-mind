84acf5b54b079eb9538bec2c3b10d02ca70ee6b6
majunior774 <mycodes774@gmail.com>
2025-03-18 08:28:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('main').addEventListener('mouseover', function() {
+    document.getElementById('highlight').style.color = '#003F2E';
+});
+document.getElementById('main').addEventListener('mouseleave', function() {
+    document.getElementById('highlight').style.color = '#2FB95D';
+});
